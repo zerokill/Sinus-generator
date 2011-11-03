a06e080e41e56f7ded96af8849e9468466ab4781
Maurice Daverveldt <maurice.daverveldt@gmail.com>
2011-11-03 15:24:30 +0100

    branch

--- main.c
+++ main.c
@@ -95,7 +95,7 @@ extern int main()
 	putstring_serial0("init done\r\n");
 	printf("printf werkt\r\n\r\n");
 	
-	printf("DSP1 - Sinus generator\r\n\r\n");
+	printf("DSP1 - Sinus generator - fork\r\n\r\n");
 		
 	PINSEL1 |= 1<<19;
 	PINSEL1 &= ~(1<<18);
@@ -109,13 +109,13 @@ extern int main()
 
 		
 		keys = Keys_Read();
-		if(keys == key_s)
+		if(keys == key_h)
 		{
 			keys = 0;
 			if (freq_deler < 511) freq_deler++;
 			printf("deler: %u \r\n",freq_deler);
 		}
-		if(keys == key_h)
+		if(keys == key_s)
 		{
 			keys = 0;
 			if (freq_deler > 1) freq_deler--;
