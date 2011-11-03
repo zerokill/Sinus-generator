d88fb702cff75b94280885e1c3062b8903f9b111
Maurice Daverveldt <maurice.daverveldt@gmail.com>
2011-11-03 15:51:08 +0100

    hallo

--- main.c
+++ main.c
@@ -96,7 +96,7 @@ extern int main()
 	printf("printf werkt\r\n\r\n");
 	
 
-	printf("DSP1 - Sinus generator - master branch\r\n\r\n");
+	printf("DSP1 - Sinus generator\r\n\r\n");
 
 		
 	PINSEL1 |= 1<<19;
